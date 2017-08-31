f75ccfd7f76013e4ba453e9d95c61daa224e28fc
Arbaz Hussain <13177578+arbazkiraak@users.noreply.github.com>
2017-09-01 02:08:35 +0530

    Create autopi.py

---
+++ autopi.py
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-key adv --keyserver hkp://keys.gnupg.net --recv-keys 7D8D0BF6
+echo 'deb http://http.kali.org/kali kali-rolling main contrib non-free'
+echo 'Add this in /etc/apt/sources.list'
+
+sudo apt-get install tightvncserver -y
+sudo apt-get install xrdp -y
+service xrdp start
